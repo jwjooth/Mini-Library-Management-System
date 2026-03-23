01400ca203ecb8498888078faf820f766a9d7120
Jordan Ferdinand <253198146+jordan-ferdinand_IMSI@users.noreply.github.com>
2026-03-23 12:27:03 +0700

    feat: configs

---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+import { appVersion } from "./utils";
+
+export const MAX_BORROW_LIMIT = 3;
+export const APP_ENV = "development";
+export default CONFIG = {
+  appName: "Mini Library System",
+  version: appVersion,
+  maxBooks: MAX_BORROW_LIMIT,
+};
