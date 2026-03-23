221ec5f4388fe93f21353b681ad12087594e88fa
Jordan Ferdinand <253198146+jordan-ferdinand_IMSI@users.noreply.github.com>
2026-03-23 11:54:22 +0700

    feat: member class

---
+++ scripts/member.js
@@ -0,0 +1,28 @@
+import { Book } from "./book";
+
+class Member {
+  constructor(name, memberId) {
+    this.name = name;
+    this.memberId = memberId;
+    this.borrowedBooks = [];
+  }
+  borrowBook(book) {
+    book.borrow();
+    this.borrowedBooks.push(book);
+    return `${this.name} borrowed "${book.title}`;
+  }
+  returnBook(book) {
+    book.returnBook();
+    this.borrowedBooks.filter((item) => item !== book);
+    return `${this.name} returned "${book.title}"`;
+  }
+  listBorrowedBooks() {
+    if (this.borrowedBooks.length !== 0) {
+      return this.borrowedBooks.map((book) => book.title);
+    } else {
+      return `No books currently borrowed`;
+    }
+  }
+}
+
+export { Member };
