b472e8ac735fa7f18ec0d3325d8a1add14b8e4f9
Jordan Ferdinand <253198146+jordan-ferdinand_IMSI@users.noreply.github.com>
2026-03-23 12:27:15 +0700

    feat: library exports

---
+++ scripts/library.js
@@ -0,0 +1,15 @@
+import { Book } from "./book";
+import { Member } from "./member";
+import { appVersion, formatDate, generateId, capitalizes } from "./utils";
+import { MAX_BORROW_LIMIT, APP_ENV } from "./config";
+
+export {
+  Book,
+  Member,
+  appVersion,
+  formatDate,
+  generateId,
+  capitalizes,
+  MAX_BORROW_LIMIT,
+  APP_ENV,
+};
